567d65d3437d8c830b9c239055a78a078ab73bdd
nyanjii <khiv.natalia@gmail.com>
2015-11-08 17:24:13 +0300

    full BD created

--- Railways/Railways/RailwayModel.Designer.cs
+++ Railways/Railways/RailwayModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\Nikita\Source\Repos\TBD\Railways\Railways\RailwayModel.edmx" включено. 
+﻿// Создание кода T4 для модели "D:\labs\TBD_CP\TBD\Railways\Railways\RailwayModel.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
