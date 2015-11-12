2b6ff4d39bd1d44eb331c0e97391b4dd03f317c2
nyanjii <khiv.natalia@gmail.com>
2015-11-12 08:34:21 +0300

    nontracingfiles

---
+++ Railways/Railways/Logic/AdminFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Railways.Logic
+{
+    public static class AdminFunctions
+    {
+        /// <summary>
+        /// Добавление нового сотрудника в систему
+        /// </summary>
+        /// <param name="fullName"></param>
+        /// <param name="passportNum"></param>
+        /// <param name="adminRights"></param>
+        public static void RegisterEmployee(String fullName, String password, String adminRights)
+        {
+            var newEmp = new Employee();
+            newEmp.Fullname = fullName;
+            newEmp.Password = Utils.EncryptString(password);
+            newEmp.AdminRights = int.Parse(adminRights);
+
+            SystemObjects.EmpList.Add(newEmp);
+        }
+    }
+}
