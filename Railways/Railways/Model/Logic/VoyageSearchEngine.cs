61450de043ac82de37baec7a50daab9b918b0395
nyanjii <khiv.natalia@gmail.com>
2015-11-28 15:48:39 +0300

    +missing files

---
+++ Railways/Railways/Model/Logic/VoyageSearchEngine.cs
@@ -0,0 +1,17 @@
+﻿using Railways.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Railways.Model.Logic
+{
+    public static class VoyageSearchEngine
+    {
+        public static void FindVoyages(String depStationName, String arStationName, DateTime depDate)
+        {
+            var voyagesForThisDate = ContextKeeper.Voyages.Where(v => v.DepartureDateTime == depDate);
+        }
+    }
+}
